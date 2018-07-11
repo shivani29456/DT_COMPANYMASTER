6ca70ab351136ecfbb3bf41234cab95149794e04
shivani29456 <shivani29456@gmail.com>
2018-07-11 11:34:26 +0530

    Caching Management

--- src/main/java/com/ril/donation/DtCompanymasterApplication.java
+++ src/main/java/com/ril/donation/DtCompanymasterApplication.java
@@ -2,11 +2,27 @@ package com.ril.donation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 
 @SpringBootApplication
 public class DtCompanymasterApplication {
 
+	
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {
+	    return new JedisConnectionFactory();
+	}
+	
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+	    final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+	    template.setConnectionFactory(jedisConnectionFactory());
+	    return template;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DtCompanymasterApplication.class, args);
 	}
